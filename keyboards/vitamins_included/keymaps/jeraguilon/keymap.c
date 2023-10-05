a3b2b2c355d1525f827179375620753d10d7988f
Jeremy Aguilon <jaguilon@hudson-trading.com>
2023-10-05 11:46:56 -0400

    Change left to lower

--- keyboards/vitamins_included/keymaps/jeraguilon/keymap.c
+++ keyboards/vitamins_included/keymaps/jeraguilon/keymap.c
@@ -20,6 +20,8 @@ enum custom_keycodes {
 };
 
 #define SPC_LWR LT(_LOWER, KC_SPC)
+#define LFT_LWR LT(_LOWER, KC_LEFT)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -38,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(_NAV, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,        KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_ESC,               KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,        KC_J,         KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,        KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-  KC_LCTRL,             KC_LGUI, KC_LALT, KC_MENU, LOWER,   MNG,     SPC_LWR,    SPC_LWR,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LCTRL,             KC_LGUI, KC_LALT, KC_MENU, LOWER,   MNG,     SPC_LWR,    SPC_LWR,  LOWER, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -56,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN,
   _______,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,  KC_COMM,  KC_DOT, _______,  KC_PGUP,
-  TG_NKRO, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  TG_NKRO, _______, _______, _______, _______, _______, _______, _______,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
 ),
 
 
